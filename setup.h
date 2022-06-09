a091972ae98876301e3e8a5d2eb7d7bb6df3be90
GnomeNeosam <55002566+GnomeNeosam@users.noreply.github.com>
2022-06-09 08:20:13 +0200

    Create setup.h

---
+++ setup.h
@@ -0,0 +1,9 @@
+#define WIFI_SSID "YOUR SSID"
+#define WIFI_PASSWORD "YOUR PASSPHRASE"
+
+// MQTT Broker
+#define MQTT_HOST IPAddress(192, 168, 1, x)
+#define MQTT_PORT 1883
+#define MQTT_USER "YOUR LOGIN"
+#define MQTT_PWD "YOUR PASSWORD"
+#define MQTT_TOPIC "TOPIC_TO_USE"
